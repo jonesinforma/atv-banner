a24d71a121851fdc837a08c089cc06d4456b4e6d
Michael Jones <michael.jones@informa.com>
2023-02-06 14:07:43 +0000

    Revision to message.

--- js/banner-and-popup-pp.js
+++ js/banner-and-popup-pp.js
@@ -3,7 +3,7 @@
 
 var bannerExists = true,
 	popupExists = false,
-	bannerEdition = "2023-02-02-04";
+	bannerEdition = "2023-02-06-02";
 
 var $setCookieButtons, $slides, $dots, $dotContainer, $firstSlide, $playPause, $bannerBox, bannerTimer, dotTimer, numBanners, $parentBody, $popupIframe, $parentBanner;
 var bannerHeight = 130,
